ba35f83fcf7470bbae402005ef58fb8472a39b5a
Nimesha07 <waruninimesha1234@gmail.com>
2025-05-15 23:49:25 +0530

    Improve mobile navigation and title size

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,8 +19,8 @@ function Navbar() {
   return (
     <AppBar position="static" className="bg-black bg-opacity-50">
       <Container maxWidth="lg">
-        <Toolbar className="justify-between">
-          <Link to="/" className="text-xl md:text-2xl font-bold text-white no-underline">
+        <Toolbar className="justify-between py-2">
+          <Link to="/" className="text-2xl md:text-3xl font-bold text-white no-underline hover:text-gray-300 transition-colors">
             Gurupata Hendewa
           </Link>
           
@@ -31,29 +31,69 @@ function Navbar() {
                 color="inherit"
                 aria-label="menu"
                 onClick={handleMenu}
+                className="text-white hover:text-gray-300"
               >
-                <MenuIcon />
+                <MenuIcon className="text-2xl" />
               </IconButton>
               <Menu
                 anchorEl={anchorEl}
                 open={Boolean(anchorEl)}
                 onClose={handleClose}
                 className="mt-12"
+                PaperProps={{
+                  className: "bg-black bg-opacity-90 backdrop-blur-sm"
+                }}
               >
-                <MenuItem component={Link} to="/" onClick={handleClose}>Home</MenuItem>
-                <MenuItem component={Link} to="/about" onClick={handleClose}>About Us</MenuItem>
-                <MenuItem component={Link} to="/contact" onClick={handleClose}>Contact</MenuItem>
+                <MenuItem 
+                  component={Link} 
+                  to="/" 
+                  onClick={handleClose}
+                  className="text-white hover:bg-gray-800 text-lg py-3"
+                >
+                  Home
+                </MenuItem>
+                <MenuItem 
+                  component={Link} 
+                  to="/about" 
+                  onClick={handleClose}
+                  className="text-white hover:bg-gray-800 text-lg py-3"
+                >
+                  About Us
+                </MenuItem>
+                <MenuItem 
+                  component={Link} 
+                  to="/contact" 
+                  onClick={handleClose}
+                  className="text-white hover:bg-gray-800 text-lg py-3"
+                >
+                  Contact
+                </MenuItem>
               </Menu>
             </>
           ) : (
             <div className="space-x-4">
-              <Button color="inherit" component={Link} to="/">
+              <Button 
+                color="inherit" 
+                component={Link} 
+                to="/"
+                className="text-lg hover:text-gray-300"
+              >
                 Home
               </Button>
-              <Button color="inherit" component={Link} to="/about">
+              <Button 
+                color="inherit" 
+                component={Link} 
+                to="/about"
+                className="text-lg hover:text-gray-300"
+              >
                 About Us
               </Button>
-              <Button color="inherit" component={Link} to="/contact">
+              <Button 
+                color="inherit" 
+                component={Link} 
+                to="/contact"
+                className="text-lg hover:text-gray-300"
+              >
                 Contact
               </Button>
             </div>
